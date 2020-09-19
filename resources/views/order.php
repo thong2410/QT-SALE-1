5e0ce9f5d13978b06e2b8f553359161794d3e01c
thong2410 <nguyenvietthong2410@gmail.com>
2020-09-19 21:22:24 +0700

    tai file len

---
+++ resources/views/order.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ĐƠN Hàng</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+
+<section class="container">
+	<header class="card-heading">
+        DANH SÁCH HÓA ĐƠN
+    </header>
+	<table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col" >id user</th>
+			<th scope="col" >số điện thoại</th>
+			<th scope="col">địa chỉ</th>
+			<th scope="col" >tên</th>
+			<th scope="col" >tiền</th>
+			<th scope="col" >ngày mua</th>
+			<th scope="col" >ghi chú</th>
+			<th>action</th>
+
+          </tr>
+        </thead>
+        <tbody>
+            <td >id user</td>
+			<td >SĐT</td>
+			<td >địa chỉ</td>
+			<td >tên</td>
+			<td >tiền</td>
+            <td >ngày mua</td>
+            <td >ghi chú</td>
+
+			<td style="color: black">
+				<a class="btn btn-outline-primary"  role="button"
+					style="color: green">Chi tiết</a>
+				<a class="btn btn-outline-primary"  role="button"
+					style="color: green">Xóa</a>
+			</td>
+
+                </tbody>
+            </table>
+
+</section>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
