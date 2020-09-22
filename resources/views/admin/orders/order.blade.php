d907f197fe36aaba5e53e6a70b0d78b449be6b49
thong2410 <nguyenvietthong2410@gmail.com>
2020-09-22 15:20:53 +0700

    update master

--- resources/views/admin/orders/order.blade.php
+++ resources/views/admin/orders/order.blade.php
@@ -1,19 +1,11 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Đơn Hàng</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('admin.layout.master')
+@section('tile','order')
+@section('content')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-
-<div class="container">
+<div class="bg-light" style="height: 800px; width: 80%; float: right;">
+    <div class="headersp">
 	<header class="card-heading col-6">
-        DANH SÁCH HÓA ĐƠN
+      <h1>  DANH SÁCH HÓA  </h1>
     </header>
     <table class="table align-items-center table-flush">
         <thead class="thead-light">
@@ -30,25 +22,18 @@
           </tr>
         </thead>
         <tbody>
-            <th class="row" >id user</td>
-			<td >SĐT</td>
-			<td >địa chỉ</td>
-			<td >tên</td>
-			<td >tiền</td>
-            <td >ngày mua</td>
-            <td >ghi chú</td>
+            <th class="row" >1</td>
+			<td >0762644516</td>
+			<td >Đà Nẵng</td>
+			<td >NGuyễn Viết Thông</td>
+			<td >12.000.000</td>
+            <td >22-9-2020</td>
+            <td >không có</td>
             <td>
-           <a href=""> <button class="btn btn-primary">Chi tiết</button></a>
+           <a href="orderdetails"> <button class="btn btn-primary">Chi tiết</button></a>
             <button class="btn btn-primary">xóa</button>
             </td>
                 </tbody>
             </table>
         </div>
- <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
-
+        @endsection
--- resources/views/admin/orders/order_details.blade.php
+++ resources/views/admin/orders/order_details.blade.php
@@ -1,51 +1,34 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Đơn Hàng</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-
-<div class="container">
+@extends('admin.layout.master')
+@section('tile','orderdetails')
+@section('content')
+<div class="bg-light" style="height: 800px; width: 80%; float: right;">
+    <div class="headersp">
 	<header class="card-heading col-6">
-        DANH SÁCH HÓA ĐƠN
+        <h1>DANH SÁCH HÓA ĐƠN</h1>
     </header>
     <table class="table align-items-center table-flush">
         <thead class="thead-light">
           <tr>
-            <th scope="col" >id user</th>
-			<th scope="col" >số điện thoại</th>
-			<th scope="col">địa chỉ</th>
-			<th scope="col" >tên</th>
-			<th scope="col" >tiền</th>
-			<th scope="col" >ngày mua</th>
+            <th scope="col" >id order</th>
+			<th scope="col" >id product</th>
+
+            <th scope="col" >Name Product</th>
+            <th scope="col">Quantity</th>
 			<th scope="col" >ghi chú</th>
-			<th>action</th>
+
 
           </tr>
         </thead>
         <tbody>
-            <th class="row" >id user</td>
-			<td >SĐT</td>
-			<td >địa chỉ</td>
-			<td >tên</td>
-			<td >tiền</td>
-            <td >ngày mua</td>
-            <td >ghi chú</td>
-           
+            <th class="row" >1</th>
+			<td >1</td>
+
+            <td>Điện Thoại ip12</td>
+            <td>22.000.000</td>
+			<td>không có</td>
+
                 </tbody>
             </table>
         </div>
- <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
 
+        @endsection
